654ad1747fe1389e2e4f321fddb57d27357cc364
Cute <Cute@DESKTOP-QCOA7R0>
2023-06-01 21:18:36 +0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            //Console.Write("Barev yerkir");
+            //Console.WriteLine("Barev Hayastan");
+
+            //byte t1 = 254;
+            //byte t2 = 254;
+            //int t3 = t1 + t2;
+            //Console.WriteLine(t3);
+            //Console.WriteLine("_____________");
+            //sbyte sb1 = 125;
+            //sbyte sb2 = 110;
+
+            //int y = 468488;
+            //int yy = 45464565;
+            //int o = y + yy;
+            //Console.WriteLine(o);
+
+            //float ff = 45.66f;
+            //double dd = 454.658;
+            //decimal ddd = 46.554564m;
+
+
+            //string t = "bchjsygdcy";
+            //string tt = "jnjvskjn v";
+
+            //string r = t + tt;
+            //Console.WriteLine(r);
+
+            //int c = 66;
+            //int cc = 1000;
+            //Console.WriteLine(c+cc);
+
+            //bool b = true;
+            //bool bb = false;
+
+            //if (c>cc) {
+            //    Console.WriteLine("c n mecaca deic");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("c poqra d ic");
+            //}
+
+            Console.Write("X = ");
+            string t = Console.ReadLine();
+            Console.Write("Y = ");
+            string tt = Console.ReadLine();
+            Console.Write("Sum = "+t+tt);
+            Console.ReadLine();
+
+
+        }
+    }
+}
